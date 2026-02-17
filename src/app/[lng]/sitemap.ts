a9da9067ea933f01bfd7c603a8053e81b6d127d0
a-kouki <koukitech5@gmail.com>
2026-02-17 04:44:52 -0300

    Update sitemap.ts

--- src/app/[lng]/sitemap.ts
+++ src/app/[lng]/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next'
  
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseURL = 'http://localhost:3000/'
+  const baseURL = 'https://koukiwebservice.com'
   
   return [
     {
